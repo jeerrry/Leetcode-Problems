ae6b0167a1026837a9f355b4103a50c652c3bf68
Jerry <s.uzairtariq@gmail.com>
2023-07-09 02:00:32 +0500

    Time: 4 ms (78.64%), Space: 43.6 MB (49.89%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,22 +1,39 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.length];
-        Arrays.fill(result, -1);
+        var map = new HashMap<Integer, Integer>();
+        var result = new int[nums1.length];
+        var stack = new Stack<Integer>();
         
         for(int i=0; i<nums1.length; i++){
-            int currentNumber = nums1[i];
-            boolean found = false;
-            
-            for(int j=0; j<nums2.length; j++){
-                if(!found && nums2[j] == currentNumber) {
-                    found = true;
+            map.put(nums1[i], i);
+        }
+        
+        for(int num : nums2) {
+            if(map.containsKey(num)){
+                // Process Stack
+                while(stack.size() > 0 && stack.peek() < num){
+                    int stackValue = stack.pop();
+                    int index = map.get(stackValue);
+                    result[index] = num;
                 }
                 
-                if(found && nums2[j] > currentNumber) {
-                    result[i] = nums2[j];
-                    break;
+                stack.push(num);
+                
+            }else {
+                // Process Stack
+                
+                while(stack.size() > 0 && stack.peek() < num){
+                    int stackValue = stack.pop();
+                    int index = map.get(stackValue);
+                    result[index] = num;
                 }
             }
+        }
+        
+        while(stack.size() > 0){
+            int stackValue = stack.pop();
+            int index = map.get(stackValue);
+            result[index] = -1;
         }
         
         return result;
