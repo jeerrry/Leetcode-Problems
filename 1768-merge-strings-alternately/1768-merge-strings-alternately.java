dd515bfa7da2f37a0a3381cfda2c0a57f6626f27
Jerry <s.uzairtariq@gmail.com>
2023-08-02 23:32:44 +0500

    Time: 1 ms (77.91%), Space: 41 MB (46.61%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int pointer1 = 0;
+        int pointer2 = 0;
+        StringBuilder result = new StringBuilder();
+        
+        while(
+        pointer1 < word1.length()
+        && pointer2 < word2.length()) {
+            char a = word1.charAt(pointer1++);
+            char b = word2.charAt(pointer2++);
+            
+            result.append(a + "");
+            result.append(b + "");
+        }
+        
+        while(pointer1 < word1.length()) {
+            result.append(word1.charAt(pointer1++));
+        }
+        
+        while(pointer2 < word2.length()) {
+            result.append(word2.charAt(pointer2++));
+        }
+        
+        return result.toString();
+    }
+}
