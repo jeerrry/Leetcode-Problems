f67873964ee7c1c2c3cab6820d34ac803cfca4cd
Jerry <s.uzairtariq@gmail.com>
2023-08-21 23:38:06 +0500

    Time: 5 ms (62.24%), Space: 43.7 MB (38.65%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,67 +1,38 @@
 class Solution {
     public String simplifyPath(String path) {
         Stack<String> stack = new Stack<>();
-        stack.push("/");
-        
-        for(int i=0; i<path.length();) {
-            StringBuilder input = new StringBuilder();
-            input.append(""+path.charAt(i));
-            
-            
-            switch(input.toString()) {
-                case "/":
-                    if(!stack.peek().equals("/")){
-                        if(i != path.length()-1){
-                            stack.push("/");
-                        }
-                    }
-                    i++;
-                    break;
-                case ".":
-                    i++;
-                    while(i < path.length()){
-                        char current = path.charAt(i);
-                        if(current == '/') break;
-                        else {
-                            input = new StringBuilder(current + input.toString());
-                            i++;
-                        }
-                    }
-                    
-                    if(input.toString().equals(".")) {
-                        continue;
-                    }else if(input.toString().equals("..")){
-                        if(stack.size() > 1){
-                            stack.pop();
-                            stack.pop();
-                        }
-                    }else{
-                        stack.push(input.toString());
-                    }
-                    break;
-                default:
-                    i++;
-                    while(i < path.length()){
-                        char current = path.charAt(i);
-                        if(current == '/') break;
-                        else {
-                            input = new StringBuilder(current + input.toString());
-                            i++;
-                        }
-                    }
-                    
-                    stack.push(input.toString());
-                    break;
-            }            
+        StringBuilder curr = new StringBuilder();
+
+        String newPath = path + "/";
+
+        for(int i=0;i<newPath.length();i++) {
+            char ch = newPath.charAt(i);
+
+            if(ch == '/') {
+                if(curr.toString().equals("..")) {
+                    if(!stack.isEmpty()) {
+                        stack.pop();
+                    } 
+                } else if(!curr.isEmpty() && !curr.toString().equals(".")) {
+                    stack.push(curr.toString());
+                }
+
+                curr = new StringBuilder();
+            } else {
+                curr.append(ch);
+            }
         }
-        
-        StringBuilder result = new StringBuilder();
-        
-        if(stack.size() > 1 && stack.peek().equals("/"))stack.pop();
-        while(stack.size() > 0) {
-            result.append(stack.pop());
+
+        curr = new StringBuilder();
+
+        while(!stack.isEmpty()) {
+            curr.insert(0, "/" + stack.pop());
         }
-        
-        return result.reverse().toString();
+
+        if(curr.length() == 0) {
+            curr.append('/');
+        }
+
+        return curr.toString();
     }
 }
