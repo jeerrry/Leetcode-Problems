45cf05ff45bd2e41793f4fb7973893cb9b53cb3a
Jerry <s.uzairtariq@gmail.com>
2023-08-22 01:48:30 +0500

    Time: 0 ms (100.00%), Space: 40.1 MB (96.97%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,63 +1,33 @@
 class Solution {
+    
+    int index = 0; 
+    
     public String decodeString(String s) {
-        Stack<String> stack = new Stack<>();
         
-        StringBuilder contextString = new StringBuilder();
-        for(int i=0; i<s.length(); i++){
-            char current = s.charAt(i);
+        StringBuilder decoded = new StringBuilder();
+        while (index < s.length() && s.charAt(index) != ']') {
             
-            if(Character.isDigit(current) || current == '[' || current == ']'){
-                if(contextString.length() > 0) {
-                    stack.push(contextString.toString());
-                    contextString = new StringBuilder();
-                }
-            }
+            // character is a letter of encoded
+            if (!Character.isDigit(s.charAt(index))) decoded.append(s.charAt(index++));
             
-            if(Character.isDigit(current)) {
-                String pushedNum = "";
-                if(stack.size() > 0){
-                    boolean isNumeric = stack.peek().chars().allMatch( Character::isDigit );
-                    if(isNumeric){
-                        pushedNum = stack.pop() + current;
-                        stack.push(pushedNum);
-                    }else {
-                        stack.push(current+"");
-                    }
-                }else {
-                    stack.push(current+"");
-                }
-            }else if(Character.isLetter(current)){
-                contextString.append(current);
+            // character is number or [ ]
+            else {
+                int k = 0;
                 
-                if(i == s.length() - 1){
-                    stack.push(contextString.toString());
-                }
-            }else {
-                if(current == '[') {
-                    stack.push(current+"");
-                }else {
-                    contextString = new StringBuilder();
-                    var temp = new StringBuilder();
-                    while(!stack.peek().equals("[")) {
-                        temp.insert(0, stack.pop());
-                    }
-                    
-                    stack.pop();
-                    int count = Integer.parseInt(stack.pop());
-                    for(int j=1; j<=count; j++){
-                        contextString.append(temp.toString());
-                    }
-                    stack.push(contextString.toString());
-                    contextString = new StringBuilder();
-                }
+                // case: number
+                while (index < s.length() && Character.isDigit(s.charAt(index))) k = k * 10 + s.charAt(index++) - '0';
+                
+                // case: [
+                index++;
+                String answer = decodeString(s);
+                
+                // case: ]
+                index++;
+                
+                // add k*encoded to decoded
+                while (k-- > 0) decoded.append(answer);
             }
         }
-        
-        contextString = new StringBuilder();
-        while(stack.size() > 0) {
-            contextString.insert(0, stack.pop());
-        }
-        
-        return contextString.toString();
+        return new String(decoded);
     }
 }
