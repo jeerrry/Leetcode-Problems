ecdbe9d62f3e3c3c91a8cda615f3548799dc715c
Jerry <s.uzairtariq@gmail.com>
2023-09-10 14:37:17 +0500

    Time: 98 ms (65.43%), Space: 57.3 MB (56.88%) - LeetHub

--- 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.java
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.java
@@ -1,20 +1,29 @@
 class Solution {
-    public int maximumRemovals(String s, String p, int[] removable) {
-        int l = 0, r = removable.length;
-        int[] map = new int[s.length()];
-        Arrays.fill(map, removable.length);
-        for (int i = 0; i < removable.length; ++i)
-             map[removable[i]] = i;    
-        while (l < r) {
-            int m = (l + r + 1) / 2, j = 0;
-            for (int i = 0; i < s.length() && j < p.length(); ++i)
-                if (map[i] >= m && s.charAt(i) == p.charAt(j))
-                    ++j;
-            if (j == p.length())
-                l = m;
-            else
-                r = m - 1;
+    public int maximumRemovals(String s, String p, int[] rem) {
+        int low = 0;
+        int high = rem.length;
+        int [] map = new int[s.length()];
+        Arrays.fill(map, rem.length);
+        for(int i=0; i<rem.length; i++) {
+            map[rem[i]] = i;
         }
-        return l;
+        
+        while(low < high) {
+            int mid = low + (high - low + 1) / 2;
+            int j = 0;
+            for(int i=0; i<s.length() && j<p.length(); i++) {
+                if(map[i] >= mid && s.charAt(i) == p.charAt(j)){
+                    j++;
+                }
+            }
+            
+            if(j == p.length()){
+                low = mid;
+            }else{
+                high = mid - 1;
+            }
+        }
+        
+        return low;
     }
 }
