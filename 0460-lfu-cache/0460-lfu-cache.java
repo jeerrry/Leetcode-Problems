a40afa6fae4f0d4fc434d93e4c753ddecdc9d370
Jerry <s.uzairtariq@gmail.com>
2023-09-17 17:41:57 +0500

    Time: 43 ms (97.79%), Space: 127.6 MB (15.44%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.java
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -1,82 +1,118 @@
+class Node{
+	int key;
+	int val;
+	Node next;
+	Node prev;
+	int freq=1;
+	Node(int k,int v){
+		key=k;
+		val=v;
+	}
+}
+class DoublyLinkedList{
+	Node head;
+	Node tail;
+	DoublyLinkedList(){
+		head=new Node(-1,-1);
+		tail=new Node(-1,-1);
+		head.next=tail;
+		tail.prev=head;
+	}
+	void addNode(Node v){
+		Node next=head.next;
+		head.next=v;
+		v.prev=head;
+		head.next=v;
+		v.next=next;
+		next.prev=v;
+	}
+	Node removeNode(){
+		Node node=tail.prev;
+		node.prev.next=tail;
+		tail.prev=node.prev;
+		return node;
+	}
+	Node removeNode(Node v){
+		Node prev=v.prev;
+		Node next=v.next;
+		prev.next=next;
+		next.prev=prev;
+		return v;
+	}
+	boolean isEmpty(){
+		if(head.next==tail)
+			return true;
+		return false;
+	}
+}
 class LFUCache {
-    class Node{
-        int k,v,f;
-        public Node(int k,int v){
-            this.k=k;
-            this.v=v;
-            this.f=1;
-        }
-    }
-    
-    int capacity,minFrequency;
-    //key,Node
-    Map<Integer,Node> map;
-    //frequency, key
-    Map<Integer,Set<Integer>> lruMap;
-
+    HashMap<Integer,DoublyLinkedList> freqList=new HashMap<Integer,DoublyLinkedList>();
+	HashMap<Integer,Node> lfuCache =new HashMap<Integer,Node>(); 
+	int capacity;
+	int minFreq;
     public LFUCache(int capacity) {
         this.capacity=capacity;
-        this.minFrequency=1;
-        map=new HashMap<>();
-        lruMap=new HashMap<>();
+        minFreq=1;
     }
-    
     public int get(int key) {
-        if(!map.containsKey(key))
-            return -1;
-        
-        Node n=map.get(key);        
-        update(n);        
-        return n.v;                        
-    }
-    
-    private void update(Node n){        
-        
-        //update frequency lru map
-        int frequency=n.f;      
-        lruMap.get(frequency).remove(n.k);                
-        
-        //update frequency
-        if(lruMap.get(frequency).isEmpty()){
-            lruMap.remove(frequency);
-            //update min frequency
-            if(frequency==minFrequency)
-                minFrequency++;
-        }                
-        lruMap.putIfAbsent(frequency+1,new LinkedHashSet<>());
-        lruMap.get(frequency+1).add(n.k);                  
-        
-        n.f++;        
+        if(lfuCache.get(key)==null)
+        	return -1;
+       	Node v=lfuCache.get(key);
+       	freqList.get(v.freq).removeNode(v);
+       	if(freqList.get(v.freq).isEmpty()){
+       		if(minFreq==v.freq){
+                minFreq=v.freq+1;
+            }
+       	}
+       	v.freq+=1;
+       	if(freqList.get(v.freq)==null){
+       		DoublyLinkedList d=new DoublyLinkedList();
+       		d.addNode(v);
+       		freqList.put(v.freq,d);
+       	}
+       	else{
+       		freqList.get(v.freq).addNode(v);
+       	}
+       	return v.val;
     }
     
     public void put(int key, int value) {
         if(capacity==0)
             return;
-        
-        if(map.containsKey(key)){
-            Node n=map.get(key); 
-            n.v=value;
-            update(n);            
-        }else{        
-            //remove min frequency/lru when capacity full
-            if(map.size()==capacity){                
-                
-                int k=lruMap.get(minFrequency).iterator().next();
-                map.remove(k);
-                
-                lruMap.get(minFrequency).remove(k);                
-                if(lruMap.get(minFrequency).isEmpty()){
-                    lruMap.remove(minFrequency);
-                }                                
-            }
-            
-            Node n=new Node(key,value);
-            map.put(key,n);
-            
-            minFrequency=1;
-            lruMap.putIfAbsent(minFrequency,new LinkedHashSet<>());
-            lruMap.get(minFrequency).add(key);                
-        }            
-                    
+        if(lfuCache.get(key)!=null){
+        	Node v=lfuCache.get(key);
+        	freqList.get(v.freq).removeNode(v);
+        	if(freqList.get(v.freq).isEmpty()){
+                if(minFreq==v.freq)
+        		minFreq=v.freq+1;
+        	}
+	       	v.freq+=1;
+	       	if(freqList.get(v.freq)==null){
+	       		 DoublyLinkedList d=new DoublyLinkedList();
+	       		d.addNode(v);
+	       		freqList.put(v.freq,d);
+	       	}
+	       	else{
+	       		freqList.get(v.freq).addNode(v);
+	       	}
+	       	v.val=value;
+	      }
+	      else{
+	     		if(lfuCache.size()==capacity){
+	     			Node v=freqList.get(minFreq).removeNode();
+	     			lfuCache.remove(v.key);			
+	     		}
+	     		Node newNode = new Node(key,value);
+	     		lfuCache.put(key,newNode);
+	     		if(freqList.get(1)!=null){
+	     			freqList.get(1).addNode(newNode);
+	     		}
+	     		else{
+	     			DoublyLinkedList d=new DoublyLinkedList();
+	     			d.addNode(newNode);
+	     			freqList.put(1,d);
+	     		}
+               minFreq=1;
+	      }
     }
 }
