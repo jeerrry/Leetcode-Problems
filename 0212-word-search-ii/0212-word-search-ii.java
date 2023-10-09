c00694bb415d29c15163ae56dfbde0883a50101f
Jerry <s.uzairtariq@gmail.com>
2023-10-09 08:50:21 +0500

    Time: 150 ms (68.69%), Space: 44.1 MB (68.48%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        TrieNode trie = buildTrie(words);
+        List<String> result = new ArrayList<>();
+        
+        // Apply DFS on matrix
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
+                dfs(i, j, board, trie, result);
+            }
+        }
+        
+        return result;
+    }
+    
+    private void dfs(int i, int j, char[][]board, TrieNode root, List<String> result) {
+        if(i < 0 || j < 0 || i == board.length || j == board[0].length) return;
+        
+        char c = board[i][j];
+        if(c == '#' || root.nodes[c - 'a'] == null) return;
+        
+        TrieNode p = root.nodes[c - 'a'];
+        
+        if(p.word != null) {
+            result.add(p.word);
+            p.word = null;
+        }
+        
+        board[i][j] = '#';
+        
+        dfs(i - 1, j, board, p, result);
+        dfs(i + 1, j, board, p, result);
+        dfs(i, j - 1, board, p, result);
+        dfs(i, j + 1, board, p, result);
+        
+        board[i][j] = c;
+    }
+    
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for(String word : words) {
+            TrieNode tempPointer = root;
+            for(char c : word.toCharArray()) {
+                if(tempPointer.nodes[c - 'a'] == null) {
+                    tempPointer.nodes[c - 'a'] = new TrieNode();
+                }
+                
+                tempPointer = tempPointer.nodes[c - 'a'];
+            }
+            tempPointer.word = word;
+        }
+        
+        return root;
+    }
+    
+    public static class TrieNode {
+        public TrieNode[] nodes = new TrieNode[26];
+        public String word = null;
+    }
+}
