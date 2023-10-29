481856bfc1d770b371d8433f8e552e9bc3446277
Jerry <s.uzairtariq@gmail.com>
2023-10-29 18:14:48 +0500

    Time: 454 ms (10.29%), Space: 44 MB (15.00%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
+                if(existHelper(board, word, i, j)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean existHelper(char[][] board, String word, int row, int col) {
+        if(word.length() == 0) {
+            return true;
+        }
+        
+        if(row < 0 || row == board.length || col < 0 || col == board[0].length) return false;
+        
+        char current = word.charAt(0);
+        
+        if(current != board[row][col]) return false;
+        
+        board[row][col] = '0';
+        boolean result = existHelper(board, word.substring(1), row, col + 1)
+            || existHelper(board, word.substring(1), row, col - 1)
+            || existHelper(board, word.substring(1), row + 1, col)
+            || existHelper(board, word.substring(1), row - 1, col);
+        board[row][col] = current;
+        
+        return result;
+    }
+}
