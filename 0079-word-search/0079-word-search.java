69676c5042af20508384933c47557573415168e7
Jerry <s.uzairtariq@gmail.com>
2023-10-29 18:15:51 +0500

    Time: 457 ms (10.15%), Space: 43.8 MB (20.71%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,7 +2,8 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i=0; i<board.length; i++) {
             for(int j=0; j<board[0].length; j++) {
-                if(existHelper(board, word, i, j)) {
+                char current = board[i][j];
+                if(current == word.charAt(0) && existHelper(board, word, i, j)) {
                     return true;
                 }
             }
