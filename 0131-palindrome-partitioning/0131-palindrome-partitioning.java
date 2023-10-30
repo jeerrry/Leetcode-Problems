dfe91cc6ba0629e77645214956069339d954c3ef
Jerry <s.uzairtariq@gmail.com>
2023-10-30 08:28:33 +0500

    Time: 24 ms (5.90%), Space: 68.8 MB (5.00%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,43 +1,26 @@
-class Solution {
+public class Solution {
+
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        
-        partitionHelper(s, "", result);
-        
-        return result;
+        List<List<String>> res = new ArrayList<List<String>>();
+        if (s.equals("")) {
+            res.add(new ArrayList<String>());
+            return res;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (isPalindrome(s, i + 1)) {
+                for (List<String> list : partition(s.substring(i + 1))) {
+                    list.add(0, s.substring(0, i + 1));
+                    res.add(list);
+                }
+            }
+        }
+        return res;
     }
-    
-    private void partitionHelper(String input, String ssf, List<List<String>> result) {
-        if(input.length() == 0) {
-            if(ssf.length() > 0) {
-                result.add(Arrays.asList(ssf.split(",")));
-            }
-            
-            return;
+
+    public boolean isPalindrome(String s, int n) {
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - i - 1)) return false;
         }
-        
-        for(int i=0; i<input.length(); i++) {
-            String s1 = input.substring(0, i + 1);
-            if(isPalindrome(s1)) {
-                String temp = ssf.length() == 0 ? s1 : ssf + "," + s1;
-                partitionHelper(input.substring(i + 1), temp, result);
-            }
-        }
-    }
-    
-    private boolean isPalindrome(String input) {
-        int start = 0;
-        int end = input.length() - 1;
-        
-        while(start < end) {
-            char a = input.charAt(start++);
-            char b = input.charAt(end--);
-            
-            if(a != b) {
-                return false;
-            }
-        }
-        
         return true;
     }
 }
