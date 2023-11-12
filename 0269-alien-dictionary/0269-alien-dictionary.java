68f41776253ca8349c9b25c7c9263177aeaedb39
Jerry <s.uzairtariq@gmail.com>
2023-11-12 16:55:53 +0500

    Time: 4 ms (79.37%), Space: 41.2 MB (35.80%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.java
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -1,62 +1,67 @@
 class Solution {
     public String alienOrder(String[] words) {
-        Map<Character, Set<Character>> map = new HashMap<Character, Set<Character>>();
-    Map<Character, Integer> degree = new HashMap<Character, Integer>();
-    String result = "";
-    if (words == null || words.length == 0)
-      return result;
-    for (String s : words) {
-      for (char c : s.toCharArray()) {
-        degree.put(c, 0);
-      }
-    }
-    for (int i = 0; i < words.length - 1; i++) {
-      boolean flag = false;
-      String cur = words[i];
-      String next = words[i + 1];
-      int length = Math.min(cur.length(), next.length());
-      for (int j = 0; j < length; j++) {
-        char c1 = cur.charAt(j);
-        char c2 = next.charAt(j);
-        if (c1 != c2) {
-          Set<Character> set = new HashSet<Character>();
-          if (map.containsKey(c1))
-            set = map.get(c1);
-          if (!set.contains(c2)) {
-            set.add(c2);
-            map.put(c1, set);
-            degree.put(c2, degree.get(c2) + 1);
-          }
-          flag = true;
-          break;
+        HashMap<Character, Integer> indegree = new HashMap<>();
+        for(String word : words) {
+            for(char c : word.toCharArray()) {
+                indegree.put(c, 0);
+            }
         }
-      }
-
-      if (flag == false && next.length() < cur.length()) {
+        
+        // Create Graph
+        HashMap<Character, HashSet<Character>> graph = new HashMap<>();
+        for(int i=0; i<words.length - 1; i++) {
+            String current = words[i];
+            String next = words[i + 1];
+            boolean found = false;
+                        
+            int len = Math.min(current.length(), next.length());
+            for(int j=0; j<len; j++) {
+                char c1 = current.charAt(j);
+                char c2 = next.charAt(j);
+                if (c1 != c2) {
+                  HashSet<Character> set = new HashSet<Character>();
+                  if (graph.containsKey(c1))
+                    set = graph.get(c1);
+                  if (!set.contains(c2)) {
+                    set.add(c2);
+                    graph.put(c1, set);
+                    indegree.put(c2, indegree.get(c2) + 1);
+                  }
+                  found = true;
+                  break;
+                }
+            }
+            
+            if(!found && current.length() > next.length()) return "";
+        }
+        
+        // Apply Khan's Algorithm
+        LinkedList<Character> queue = new LinkedList<>();
+        StringBuilder ans = new StringBuilder();
+        int count = 0;
+        
+        for(char ch : indegree.keySet()) {
+            if(indegree.get(ch) > 0) continue;
+            queue.addLast(ch);
+        }
+        
+        while(queue.size() > 0) {
+            char rem = queue.removeFirst();
+            ans.append(rem);
+            count++;
+            
+            if(graph.containsKey(rem)) {
+                var neighbours = graph.get(rem);
+                for(char nbr : neighbours) {
+                    indegree.put(nbr, indegree.get(nbr) - 1);
+                    if(indegree.get(nbr) > 0) continue;
+                    
+                    queue.addLast(nbr);
+                }
+            }
+        }
+        
+        if(count == indegree.size()) return ans.toString();
         return "";
-      }
-    }
-    Queue<Character> q = new LinkedList<Character>();
-    for (char c : degree.keySet()) {
-      if (degree.get(c) == 0)
-        q.add(c);
-    }
-    while (!q.isEmpty()) {
-      char c = q.remove();
-      result += c;
-      if (map.containsKey(c)) {
-        for (char c2 : map.get(c)) {
-          degree.put(c2, degree.get(c2) - 1);
-          if (degree.get(c2) == 0)
-            q.add(c2);
-        }
-      }
-    }
-
-    if (result.length() != degree.size()) {
-      return "";
-    }
-
-    return result;
     }
 }
