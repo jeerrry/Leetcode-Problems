9b2cf89c6c74c3e2a87b47420b8ca2ef364a56ae
Jerry <s.uzairtariq@gmail.com>
2023-11-17 11:58:26 +0500

    Time: 29 ms (46.52%), Space: 111.8 MB (9.56%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,85 +1,71 @@
 class Solution {
     public int maxNumEdgesToRemove(int n, int[][] edges) {
-    Arrays.sort(edges, (a, b) -> Integer.compare(b[0], a[0]));
-    int[] parenta = new int[n + 1];
-    int[] parentb = new int[n + 1];
-    int[] ranka = new int[n + 1];
-    int[] rankb = new int[n + 1];
-    int counta = 1;
-    int countb = 1;
+        int[] parenta = new int[n + 1];
+        int[] parentb = new int[n + 1];
+        int[] ranka = new int[n + 1];
+        int[] rankb = new int[n + 1];
+        
+        for(int i=0; i<=n; i++) {
+            parenta[i] = parentb[i] = i;
+        }
+        
+        Arrays.sort(edges, (a, b) -> Integer.compare(b[0], a[0]));
+        
+        int mergeda = 1;
+        int mergedb = 1;
+        int removed = 0;
+        
+        for(int[] edge : edges) {
+            if(edge[0] == 3){
+                boolean ans1 = union(edge[1], edge[2], parenta, ranka);
+                boolean ans2 = union(edge[1], edge[2], parentb, rankb);
+                
+                mergeda += (ans1 ? 1 : 0);
+                mergedb += (ans2 ? 1 : 0);
+                removed += (!ans1 && !ans2) ? 1 : 0;
+            }else if(edge[0] == 1) {
+                boolean ans1 = union(edge[1], edge[2], parenta, ranka);
+                
+                mergeda += (ans1 ? 1 : 0);
+                removed += (!ans1) ? 1 : 0;
+            }else {
+                boolean ans2 = union(edge[1], edge[2], parentb, rankb);
+                
+                mergedb += (ans2 ? 1 : 0);
+                removed += (!ans2 ? 1 : 0);
+            }
+        }
+        
+        if(mergeda != n || mergedb != n) return -1;
+        
+        return removed;
 
-    for (int i = 0; i <= n; i++) {
-      parenta[i] = i;
-      parentb[i] = i;
     }
-
-    int removedEdges = 0;
-
-    for (int[] e : edges) {
-      if (e[0] == 3) {
-        boolean mergeda = union(e[1], e[2], parenta, ranka);
-        boolean mergedb = union(e[1], e[2], parentb, rankb);
-        if (mergeda == true) {
-          counta++;
+    
+    private int find(int x, int[] parent) {
+        if(parent[x] == x) {
+            return x;
         }
-        if (mergedb == true) {
-          countb++;
-        }
-
-        if (!mergeda && !mergedb) {
-          removedEdges++;
-        }
-      } else if (e[0] == 1) {
-        boolean mergeda = union(e[1], e[2], parenta, ranka);
-        if (mergeda == true) {
-          counta++;
-        }
-
-        if (!mergeda) {
-          removedEdges++;
-        }
-      } else {
-        boolean mergedb = union(e[1], e[2], parentb, rankb);
-        if (mergedb == true) {
-          countb++;
-        }
-        if (!mergedb) {
-          removedEdges++;
-        }
-      }
+        
+        parent[x] = find(parent[x], parent);
+        return parent[x];
     }
-
-    if (countb != n || counta != n) {
-      return -1;
+    
+    private boolean union(int x, int y, int[] parent, int[] rank) {
+        int xx = find(x, parent);
+        int yy = find(y, parent);
+        
+        if(xx == yy) return false;
+        
+        if(rank[xx] > rank[yy]) {
+            parent[yy] = xx;
+        }else if(rank[yy] > rank[xx]) {
+            parent[xx] = yy;
+        }else {
+            parent[yy] = xx;
+            rank[xx] += 1;
+        }
+        
+        return true;
     }
-
-    return removedEdges;
-  }
-
-  public int find(int x, int[] parent) {
-    if (parent[x] == x) {
-      return x;
-    }
-    int temp = find(parent[x], parent);
-    parent[x] = temp;
-    return temp;
-  }
-
-  public boolean union(int x, int y, int[] parent, int[] rank) {
-    int lx = find(x, parent);
-    int ly = find(y, parent);
-
-    if (lx == ly)
-      return false;
-
-    if (rank[lx] > rank[ly]) {
-      parent[ly] = lx;
-    } else if (rank[lx] < rank[ly]) {
-      parent[lx] = ly;
-    } else {
-      parent[lx] = ly;
-      rank[ly]++;
-    }
-    return true;
-  }
 }
