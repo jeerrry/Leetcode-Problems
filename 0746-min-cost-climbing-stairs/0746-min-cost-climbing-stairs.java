3a074a5a9779ab07609ea3f47c855c7e990260a1
Jerry <s.uzairtariq@gmail.com>
2023-12-11 08:37:50 +0500

    Time: 0 ms (100.00%), Space: 43.7 MB (6.34%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -4,13 +4,10 @@ class Solution {
         result[0] = cost[0];
         result[1] = cost[1];
         
-        for(int i=2; i<=cost.length; i++) {
-            result[i] = Math.min(result[i-1], result[i-2]);
-            if(i < cost.length) {
-                result[i] += cost[i];
-            }
+        for(int i=2; i<cost.length; i++) {
+            result[i] = Math.min(result[i-1], result[i-2]) + cost[i];
         }
         
-        return result[cost.length];
+        return Math.min(result[cost.length - 1], result[cost.length - 2]);
     }
 }
