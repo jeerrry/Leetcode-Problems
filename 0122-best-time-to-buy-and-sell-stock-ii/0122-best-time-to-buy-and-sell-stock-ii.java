2624574ceb7de6150a4f80ac9bbf7e00d84b6c29
Jerry <s.uzairtariq@gmail.com>
2024-01-15 09:39:19 +0500

    Time: 1 ms (89.14%), Space: 46 MB (16.05%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,35 +1,25 @@
 class Solution {
 
-    public int maxProfit(int[] Arr) {
+    public int maxProfit(int[] prices) {
         //return maxProfitHelper(0, true, prices, new int[prices.length + 1][2]);
-        int n = Arr.length;
-        // Create a 2D array for dynamic programming with dimensions [n+1][2]
-        long[][] dp = new long[n + 1][2];
+        int n = prices.length;
+        int[][] dp = new int[n + 1][2];
 
-        // Initialize the entire dp table with -1
-        for (long[] row : dp) {
-            Arrays.fill(row, -1);
-        }
 
-        // Base condition: If we have no stocks to buy or sell, profit is 0
         dp[n][0] = dp[n][1] = 0;
-
-        long profit = 0;
-
-        // Iterate through the array in reverse to calculate the maximum profit
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                if (buy == 0) { // We can buy the stock
-                    profit = Math.max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < 2; j++) {
+                int profit = 0;
+                if(j == 0) {
+                    profit = Math.max(-prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                }else {
+                    profit = Math.max(prices[i] + dp[i + 1][0], dp[i + 1][1]);
                 }
-
-                if (buy == 1) { // We can sell the stock
-                    profit = Math.max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 1][0]);
-                }
-
-                dp[ind][buy] = profit;
+                
+                dp[i][j] = profit;
             }
         }
-        return (int)dp[0][0]; // The maximum profit is stored at dp[0][0]
+        
+        return dp[0][0];
     }
 }
