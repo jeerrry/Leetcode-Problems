b7e02e2e971e263db8215419b2d2a66fe33bcd86
Jerry <s.uzairtariq@gmail.com>
2024-01-15 09:34:30 +0500

    Time: 1 ms (89.14%), Space: 46 MB (12.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,37 +1,35 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int profit = 0;
-        boolean purchased = false;
-        int purchaseVal = -1;
-        int saleVal = -1;
-        
-        
-        for(int i=0; i<prices.length; i++) {
-            int val = prices[i];
-            
-            if(!purchased) {
-                if(purchaseVal == -1 || val < purchaseVal) {
-                    purchaseVal = val;
-                }else {
-                    purchased = true;
-                    saleVal = val;
+
+    public int maxProfit(int[] Arr) {
+        //return maxProfitHelper(0, true, prices, new int[prices.length + 1][2]);
+        int n = Arr.length;
+        // Create a 2D array for dynamic programming with dimensions [n+1][2]
+        long[][] dp = new long[n + 1][2];
+
+        // Initialize the entire dp table with -1
+        for (long[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        // Base condition: If we have no stocks to buy or sell, profit is 0
+        dp[n][0] = dp[n][1] = 0;
+
+        long profit = 0;
+
+        // Iterate through the array in reverse to calculate the maximum profit
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy == 0) { // We can buy the stock
+                    profit = Math.max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
                 }
-            }else {
-                if(saleVal == -1 || val > saleVal) {
-                    saleVal = val;
-                }else {
-                    profit += (saleVal - purchaseVal);
-                    purchased = false;
-                    saleVal = -1;
-                    purchaseVal = val;
+
+                if (buy == 1) { // We can sell the stock
+                    profit = Math.max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 1][0]);
                 }
+
+                dp[ind][buy] = profit;
             }
         }
-        
-        if(purchased) {
-            profit += (saleVal - purchaseVal);
-        }
-        
-        return profit;
+        return (int)dp[0][0]; // The maximum profit is stored at dp[0][0]
     }
 }
