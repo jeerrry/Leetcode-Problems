262b94eebf48acce9c6839f4eb7e101550cd1400
Jerry <s.uzairtariq@gmail.com>
2024-05-19 22:42:52 +0500

    Time: 0 ms (100.00%), Space: 42.2 MB (49.19%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,38 +1,18 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         if(nums.length == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
         
-        int low = 0;
-        int high = nums.length - 1;
-        
+        int low = 1;
+        int high = nums.length - 2;
         while(low <= high) {
-            int mid = low + (high - low) / 2;
-            int move = isPeakElement(nums, mid);
-            
-            if(move == 0) {
-                return mid;
-            }else if(move == -1) {
-                high = mid - 1;
-            }else {
-                low = mid + 1;
-            }
+            int mid = (low + high) / 2;
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+            if(nums[mid] < nums[mid + 1]) low = mid + 1;
+            else high = mid - 1;
         }
         
-        return low;
-    }
-    
-    private int isPeakElement(int[] nums, int i) {
-        if(i < 0 || i >= nums.length) return 0;
-        
-        int currentVal = nums[i];
-        if(i - 1 >= 0 && nums[i-1] >= currentVal) {
-            return -1;
-        }
-        
-        if(i + 1 < nums.length && nums[i + 1] >= currentVal) {
-            return 1;
-        }
-        
-        return 0;
+        return -1;
     }
 }
