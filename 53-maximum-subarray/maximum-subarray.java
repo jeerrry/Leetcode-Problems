ed361b40abb730561d3258cf8f168bc528b43e61
Jerry <s.uzairtariq@gmail.com>
2025-01-13 23:26:12 +0500

    Time: 1 ms (99.42%) | Memory: 57 MB (66.93%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,8 +3,6 @@ class Solution {
         int maxSum = Integer.MIN_VALUE;
         int currentSum = 0;
         
-        int startIndex = -1;
-        int endIndex = -1;
         for(int i=0; i<nums.length; i++) {
             int val = nums[i];
             currentSum += val;
