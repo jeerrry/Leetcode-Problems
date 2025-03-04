fc9a5f09715ac4e5e12dafdb44def6f2e7a24ab6
Jerry <s.uzairtariq@gmail.com>
2025-03-04 23:40:22 +0500

    Time: 7 ms (98.20%) | Memory: 44.6 MB (77.03%) - LeetSync

---
+++ 727-minimum-window-subsequence/minimum-window-subsequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String minWindow(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int start = 0, len = Integer.MAX_VALUE, count = 0;
+
+        for (int i = 0; i < m; i++) {
+            if (s1.charAt(i) == s2.charAt(count)) count++;
+
+            // all characters in s2 are matched. now reduce the window to the right-most part which has all the characters in s2
+            if (count == n) {
+                int j = i;
+                while (count > 0) {
+                    if (s2.charAt(count - 1) == s1.charAt(j--)) {
+                        count--;
+                    }
+                }
+                j++; // index in s1 which is the first character in s2
+
+                if (len > i - j + 1) {
+                    len = i - j + 1;
+                    start = j;
+                }
+                // move back i so it starts next search from j+1 in next iteration
+                i = j;
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : s1.substring(start, start + len);
+    }
+}
