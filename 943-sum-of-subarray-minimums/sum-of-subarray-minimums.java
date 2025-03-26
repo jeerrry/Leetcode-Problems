d5709e6084f7b94099fe2624ee55335421452527
Jerry <s.uzairtariq@gmail.com>
2025-03-27 00:49:08 +0500

    Time: 20 ms (95.50%) | Memory: 53.9 MB (5.26%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,42 +1,30 @@
-
 class Solution {
+    int MOD = (int) 1e9 + 7;
+
+    private int getElement(int[] arr, int n, int i) {
+        return (i == -1 || i == n) ? Integer.MIN_VALUE : arr[i];
+    }
+
     public int sumSubarrayMins(int[] arr) {
-        int MOD = 1000000007;
+        int n = arr.length;
+        long ans = 0;
+        // Deque is faster than Stack
+        Deque<Integer> stack = new LinkedList<>();
 
-        Stack<Integer> stack = new Stack<>();
-
-        // make a dp array of the same size as the input array `arr`
-        int[] dp = new int[arr.length];
-
-        // making a monotonic increasing stack
-        for (int i = 0; i < arr.length; i++) {
-            // pop the stack until it is empty or
-            // the top of the stack is greater than or equal to
-            // the current element
-            while (!stack.empty() && arr[stack.peek()] >= arr[i]) {
-                stack.pop();
+        // 2 sentinels: set arr[-1] & arr[n] as MIN_VALUE
+        for (int i = -1; i <= n; i++) {
+            // Find left closest element, e <= A[i]
+            while (!stack.isEmpty() && getElement(arr, n, stack.peek()) > getElement(arr, n, i)) {
+                // for each stack.pop(),
+                // i is its right boundary and stack.peek() is left boundary.
+                int currIdx = stack.pop();
+                int left = stack.peek(), right = i;
+                // calculate contribution
+                ans = (ans + (long) (currIdx - left) * (right - currIdx) * arr[currIdx]) % MOD;
             }
-
-            // either the previousSmaller element exists
-            if (stack.size() > 0) {
-                int previousSmaller = stack.peek();
-                dp[i] = dp[previousSmaller] + (i - previousSmaller) * arr[i];
-            } else {
-                // or it doesn't exist, in this case the current element
-                // contributes with all subarrays ending at i
-                dp[i] = (i + 1) * arr[i];
-            }
-            // push the current index
             stack.push(i);
         }
 
-        // Add all elements of the dp to get the answer
-        long sumOfMinimums = 0;
-        for (int count : dp) {
-            sumOfMinimums += count;
-            sumOfMinimums %= MOD;
-        }
-
-        return (int) sumOfMinimums;
+        return (int) ans;
     }
 }
