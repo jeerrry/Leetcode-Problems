85ef65b856d3a7b0caed45426841702c3d8db14a
Jerry <s.uzairtariq@gmail.com>
2025-03-31 21:07:40 +0500

    Time: 21 ms (45.34%) | Memory: 44.6 MB (35.97%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,91 +1,44 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long maxSum = sumSubarrayMaxs(nums);
-        long minSum = sumSubarrayMins(nums);
-        return maxSum - minSum;
+        return sumSubarray(nums, true) - sumSubarray(nums, false);
     }
 
-    private long sumSubarrayMins(int[] arr) {
-        int[] pse = pseMin(arr);
-        int[] nse = nseMin(arr);
-
-        long result = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int left = i - pse[i];
-            int right = nse[i] - i;
-
-            long contrib = (long) left * right * arr[i];
-            result += contrib;
-        }
-
-        return result;
-    }
-
-    private long sumSubarrayMaxs(int[] arr) {
-        int[] pge = pgeMax(arr);
-        int[] nge = ngeMax(arr);
-
-        long result = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int left = i - pge[i];
-            int right = nge[i] - i;
-
-            long contrib = (long) left * right * arr[i];
-            result += contrib;
-        }
-
-        return result;
-    }
-
-    private int[] pseMin(int[] arr) {
+    private long sumSubarray(int[] arr, boolean isMax) {
+        int n = arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
         Stack<Integer> stack = new Stack<>();
-        int[] result = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+
+        // Previous greater/smaller
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && (
+                isMax ? arr[stack.peek()] < arr[i] : arr[stack.peek()] > arr[i]
+            )) {
                 stack.pop();
             }
-            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
             stack.push(i);
         }
-        return result;
-    }
 
-    private int[] nseMin(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
-        int[] result = new int[arr.length];
-        for (int i = arr.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+        stack.clear();
+
+        // Next greater/smaller
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && (
+                isMax ? arr[stack.peek()] <= arr[i] : arr[stack.peek()] >= arr[i]
+            )) {
                 stack.pop();
             }
-            result[i] = stack.isEmpty() ? arr.length : stack.peek();
+            right[i] = stack.isEmpty() ? n : stack.peek();
             stack.push(i);
         }
-        return result;
-    }
 
-    private int[] pgeMax(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
-        int[] result = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
-                stack.pop();
-            }
-            result[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            long contrib = (long) (i - left[i]) * (right[i] - i) * arr[i];
+            sum += contrib;
         }
-        return result;
-    }
 
-    private int[] ngeMax(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
-        int[] result = new int[arr.length];
-        for (int i = arr.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
-                stack.pop();
-            }
-            result[i] = stack.isEmpty() ? arr.length : stack.peek();
-            stack.push(i);
-        }
-        return result;
+        return sum;
     }
 }
