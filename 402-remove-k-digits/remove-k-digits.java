9dcb077b4d7dbbaca27b507cace20f216bd7f530
Jerry <s.uzairtariq@gmail.com>
2025-03-31 21:28:44 +0500

    Time: 77 ms (14.54%) | Memory: 45.5 MB (67.46%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        var stack = new Stack<Integer>();
+        int removed = 0;
+        for(char c : num.toCharArray()) {
+            int val = c - '0';
+
+            if(stack.isEmpty() && val == 0) continue;
+            while(removed < k && !stack.isEmpty() && stack.peek() > val) {
+                stack.pop();
+                removed++;
+            }
+
+            stack.push(val);
+        }
+
+        while(removed < k && !stack.isEmpty()){
+            stack.pop();
+            removed++;
+        }
+
+        var result = new StringBuilder();
+        while(!stack.isEmpty()) result.insert(0,stack.pop());
+
+        while(result.length() > 0 && result.charAt(0) == '0') result.deleteCharAt(0);
+        return result.length() == 0 ? "0" : result.toString();
+    }
+}
