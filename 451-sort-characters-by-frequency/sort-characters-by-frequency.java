b5b2cd26c09abea9483d7ec552205e3b066271ce
Jerry <s.uzairtariq@gmail.com>
2025-07-13 16:24:30 +0500

    Time: 11 ms (88.67%) | Memory: 45.6 MB (66.18%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -2,20 +2,31 @@ class Solution {
     public String frequencySort(String s) {
         var map = new HashMap<Character, Integer>();
         for(char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            int current = map.getOrDefault(c, 0) + 1;
+            map.put(c, current);
         }
 
-        var pq = new PriorityQueue<Map.Entry<Character, Integer>>((a, b) -> b.getValue() - a.getValue());
-        pq.addAll(map.entrySet());
+        int maxFreq = s.length();
+        List<Character>[] buckets = new ArrayList[maxFreq + 1];
+        for(char key : map.keySet()) {
+            int freq = map.get(key);
+            if(buckets[freq] == null) {
+                buckets[freq] = new ArrayList<Character>();
+            }
 
-        var result = new StringBuilder();
-        while(!pq.isEmpty()) {
-            var val = pq.remove();
-            for(int i=0; i<val.getValue(); i++) {
-                result.append(val.getKey());
+            buckets[freq].add(key);
+        }
+
+        var builder = new StringBuilder();
+        for(int i=maxFreq; i > 0; i--) {
+            if(buckets[i] == null) continue;
+            for(char c : buckets[i]) {
+                for(int j=0; j<i;j++){
+                    builder.append(c);
+                }
             }
         }
 
-        return result.toString();
+        return builder.toString();
     }
 }
