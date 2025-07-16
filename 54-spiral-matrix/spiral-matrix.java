85e2defa275f45f0990ff88f34a5048b3b3725cb
Jerry <s.uzairtariq@gmail.com>
2025-07-16 23:12:40 +0500

    Time: 0 ms (100.00%) | Memory: 41.8 MB (15.63%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -2,36 +2,34 @@ class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         int minRow = 0;
         int minCol = 0;
-        int maxRow = matrix.length - 1;
-        int maxCol = matrix[0].length - 1;
-        int total = matrix.length * matrix[0].length;
+        int maxRow = matrix.length;
+        int maxCol = matrix[0].length;
+
+        int totalElements = maxRow * maxCol;
         int count = 0;
 
-        var result = new ArrayList<Integer>();
-        while(count < total) {
-            // Top Row
-            for(int i=minCol; i<=maxCol && count < total; i++) {
+        List<Integer> result = new ArrayList<>();
+        while(count < totalElements) {
+            // Process top row
+            for(int i=minCol; i<maxCol && count < totalElements; i++) {
                 result.add(matrix[minRow][i]);
                 count++;
             }
             minRow++;
 
-            // Right Col
-            for(int i=minRow; i<=maxRow && count < total; i++) {
-                result.add(matrix[i][maxCol]);
+            for(int i=minRow; i<maxRow && count < totalElements; i++) {
+                result.add(matrix[i][maxCol-1]);
                 count++;
             }
             maxCol--;
 
-            // Bottom Row
-            for(int i=maxCol; i>=minCol && count < total; i--) {
-                result.add(matrix[maxRow][i]);
+            for(int i=maxCol - 1; i >=minCol && count < totalElements; i--) {
+                result.add(matrix[maxRow - 1][i]);
                 count++;
             }
             maxRow--;
 
-            // Left Col
-            for(int i=maxRow; i>=minRow && count < total; i--) {
+            for(int i=maxRow - 1; i >= minRow && count < totalElements; i--){
                 result.add(matrix[i][minCol]);
                 count++;
             }
